bfce995eb59fbda1e85bddcc840cf46aa06707d0
GuiQRSM <quaresmagui20@gmail.com>
2025-10-27 16:01:27 -0300

    lib: install ESlint with a package from rocketseat

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import fastify from "fastify";
 
-const app = fastify()
+const app = fastify();
 
 app.get('/hello', () => {
-    return 'Hello World'
+    return 'Hello World, im using fastfy with Node.js'
 })
 
 app.listen({
     port: 3333
 }).then(() => {
-    console.log('HTTP Server is runnig')
+    console.log("HTTP Server is running")
 })
