eff2f26fd9077b93036aa9288f9702d92f8b1790
GuiQRSM <quaresmagui20@gmail.com>
2025-10-28 10:23:47 -0300

    add: declare client and connection

--- src/database.ts
+++ src/database.ts
@@ -1,3 +1,8 @@
 import { knex as setupKnex } from 'knex';
 
-export const kmex = setupKnex({});
+export const kmex = setupKnex({
+  client: 'sqlite',
+  connection: {
+    filename: './tmp/app.db',
+  },
+});
