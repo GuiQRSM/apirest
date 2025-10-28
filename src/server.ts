1307098f2b751514e5fd99ace3382678c40257a3
GuiQRSM <quaresmagui20@gmail.com>
2025-10-28 09:33:57 -0300

    fix: fixes ESlint issue

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify';
 
 const app = fastify();
 
 app.get('/hello', () => {
-    return 'Hello World, im using fastfy with Node.js'
-})
+  return 'Hello World, im using fastfy with Node.js';
+});
 
-app.listen({
-    port: 3333
-}).then(() => {
-    console.log("HTTP Server is running")
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server is running');
+  });
