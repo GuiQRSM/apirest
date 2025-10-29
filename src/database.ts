349099befa24f4d4f33746743c6127639ffc71c9
GuiQRSM <quaresmagui20@gmail.com>
2025-10-29 10:01:04 -0300

    refactor: separates the database settings into a separate const

--- src/database.ts
+++ src/database.ts
@@ -2,10 +2,12 @@ import knex from 'knex';
 
 const setupKnex = knex;
 
-export const knexInstance = setupKnex({
+export const config = {
   client: 'sqlite3',
   connection: {
     filename: './tmp/app.db',
   },
   useNullAsDefault: true,
-});
+};
+
+export const knexInstance = setupKnex(config);
