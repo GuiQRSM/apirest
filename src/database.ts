f518e325f2065044d2f7163051ac7bfe2c101d3d
GuiQRSM <quaresmagui20@gmail.com>
2025-10-29 10:52:17 -0300

    add: configurations for handling migrations with knex

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,19 @@
 import knex from 'knex';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Knex = import('knex').Knex;
 
 const setupKnex = knex;
 
-export const config = {
+export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
   useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
 };
 
 export const knexInstance = setupKnex(config);
