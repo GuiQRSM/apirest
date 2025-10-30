513feb879ce1baa41806c06e68f1d3656e1b34cc
GuiQRSM <quaresmagui20@gmail.com>
2025-10-30 10:06:25 -0300

    add: The up and down methods of the new migration have been configured.

--- db/migrations/20251030125928_add-session-id-to-transactional.ts
+++ db/migrations/20251030125928_add-session-id-to-transactional.ts
@@ -1,10 +1,13 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transaction', (table) => {
+    table.uuid('session_id').after('id').index();
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('session_id', (table) => {
+    table.dropColumn('session_id');
+  });
 }
-
