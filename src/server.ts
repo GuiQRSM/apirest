c711792a97dbc7dbaaaa1a9f9d02483752b999db
GuiQRSM <quaresmagui20@gmail.com>
2025-10-31 12:51:24 -0300

    refactor: encerra teste de outras queries

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,13 @@ import { knexInstance } from './database.js';
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tansactions = await knexInstance('transactions').select('*');
+  const tansactions = await knexInstance('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transações de teste',
+      amount: 1000,
+    })
+    .returning('*');
 
   return tansactions;
 });
