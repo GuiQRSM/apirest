059e9259a4f7b3efa7c4e3523fa98ff8d75264bd
GuiQRSM <quaresmagui20@gmail.com>
2025-11-03 09:46:30 -0300

    add: It calls the 'PORT' of the new environment configured with zod.

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from 'fastify';
 import crypto from 'node:crypto';
 import { knexInstance } from './database.js';
+import { env } from '../env/index.js';
 
 const app = fastify();
 
@@ -18,7 +19,7 @@ app.get('/hello', async () => {
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server is running');
