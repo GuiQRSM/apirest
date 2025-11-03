4da1d426c50990818b1668a842a7c68de9b2e6e0
GuiQRSM <quaresmagui20@gmail.com>
2025-11-03 09:46:05 -0300

    add: It calls the 'DATABASE_URL' of the new environment configured with zod.

--- src/database.ts
+++ src/database.ts
@@ -1,18 +1,14 @@
-import 'dotenv/config';
 import knex from 'knex';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import type { Knex } from 'knex';
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL env not found!');
-}
+import { env } from '../env/index.js';
 
 const setupKnex = knex;
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
