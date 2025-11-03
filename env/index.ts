dd3d740f3dec00391342aa571aa4a95068adbd2a
GuiQRSM <quaresmagui20@gmail.com>
2025-11-03 09:44:59 -0300

    add: refactor 'DATABASE_URL' and declare 'PORT' as a variable.

--- env/index.ts
+++ env/index.ts
@@ -2,7 +2,8 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
-  DATABASE_URL: z.object(),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
 });
 
 export const env = envSchema.parse(process.env);
