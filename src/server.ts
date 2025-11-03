2feeddd8bdd36ed6c65513a035f0a5e8d8925cf9
GuiQRSM <quaresmagui20@gmail.com>
2025-11-03 10:58:55 -0300

    add: this calls the GET plugin for transactions in the server file.

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,10 @@
 import fastify from 'fastify';
-import crypto from 'node:crypto';
-import { knexInstance } from './database.js';
 import { env } from '../env/index.js';
+import { transactionsRoutes } from '../routes/transactions.js';
 
 const app = fastify();
 
-app.get('/hello', async () => {
-  const tansactions = await knexInstance('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transações de teste',
-      amount: 1000,
-    })
-    .returning('*');
-
-  return tansactions;
-});
+app.register(transactionsRoutes);
 
 app
   .listen({
