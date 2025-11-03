5ca01bab7439c66c3930b95f61aa11a8665cfc22
GuiQRSM <quaresmagui20@gmail.com>
2025-11-03 10:56:44 -0300

    add: Implements the plugin for the GET route.

--- routes/transactions.ts
+++ routes/transactions.ts
@@ -1,1 +1,15 @@
-export async function transactionsRoutes(app) {}
+import { knexInstance } from '../src/database.js';
+
+export async function transactionsRoutes(app) {
+  app.get('/hello', async () => {
+    const tansactions = await knexInstance('transactions')
+      .insert({
+        id: crypto.randomUUID(),
+        title: 'Transações de teste',
+        amount: 1000,
+      })
+      .returning('*');
+
+    return tansactions;
+  });
+}
