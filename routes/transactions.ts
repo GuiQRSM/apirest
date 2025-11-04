ed560ad45d681d29c9cdf39f4e5d31869a72f881
GuiQRSM <quaresmagui20@gmail.com>
2025-11-04 10:10:57 -0300

    refactor: Define the GET route for the POST route.

--- routes/transactions.ts
+++ routes/transactions.ts
@@ -1,16 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { knexInstance } from '../src/database.js';
-import crypto from 'node:crypto';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const tansactions = await knexInstance('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title: 'Transações de teste',
-        amount: 1000,
-      })
-      .returning('*');
+  app.post('/transactions', async () => {
+    const tansactions = await knexInstance('transactions').where('amount', 1000).select('*');
 
     return tansactions;
   });
