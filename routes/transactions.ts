066bd5e0865e61eefae638bc4480afc8018226c8
GuiQRSM <quaresmagui20@gmail.com>
2025-11-04 10:30:54 -0300

    add: treatment for transaction creation

--- routes/transactions.ts
+++ routes/transactions.ts
@@ -1,16 +1,23 @@
 import { FastifyInstance } from 'fastify';
 import z from 'zod';
+import { knexInstance } from '../src/database.js';
+import { randomUUID } from 'node:crypto';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.post('/', async (request) => {
+  app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
       type: z.enum(['credit', 'debit']),
     });
 
-    const body = createTransactionBodySchema.parse(request.body);
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body);
 
-    return body;
+    await knexInstance('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+    return reply.status(201).send;
   });
 }
