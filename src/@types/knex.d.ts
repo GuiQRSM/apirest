6fd3b06705da621df908575b3f02160897d16625
GuiQRSM <quaresmagui20@gmail.com>
2025-11-06 11:45:09 -0300

    add: declares the types of the elements in the transactions table.

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,2 +1,13 @@
 // eslint-disable-next-line
 import { Knex } from 'knex';
+
+declare module 'knexInstance/types/tables' {
+  export interface Tables {
+    transactions: {
+      id: string;
+      title: string;
+      amount: number;
+      created_at: Date;
+    };
+  }
+}
