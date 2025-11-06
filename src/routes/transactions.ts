ebf8363378fcc787d6168d4e70f4c773c612b477
GuiQRSM <quaresmagui20@gmail.com>
2025-11-06 16:48:40 -0300

    add: Place the return value of the `get` const inside an object.

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -7,7 +7,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
     const transactions = await knexInstance('transactions').select();
 
-    return transactions;
+    return { transactions };
   });
 
   app.post('/', async (request, reply) => {
