5bc02a2822fff38cd16835e55e4bc82997431c1f
GuiQRSM <quaresmagui20@gmail.com>
2025-11-10 11:55:10 -0300

    add: declares the 'preHandler' on all routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -21,23 +21,35 @@ export async function transactionsRoutes(app: FastifyInstance) {
     },
   );
 
-  app.get('/:id', async (request) => {
-    const getTransactionsParamsSchema = z.object({
-      id: z.string().uuid(),
-    });
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request) => {
+      const getTransactionsParamsSchema = z.object({
+        id: z.string().uuid(),
+      });
 
-    const { id } = getTransactionsParamsSchema.parse(request.params);
+      const { id } = getTransactionsParamsSchema.parse(request.params);
 
-    const transactions = await knexInstance('transactions').where('id', id).first();
+      const transactions = await knexInstance('transactions').where('id', id).first();
 
-    return { transactions };
-  });
+      return { transactions };
+    },
+  );
 
-  app.get('/summary', async () => {
-    const summary = await knexInstance('transactions').sum('amount', { as: 'amount' }).first();
+  app.get(
+    '/summary',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async () => {
+      const summary = await knexInstance('transactions').sum('amount', { as: 'amount' }).first();
 
-    return { summary };
-  });
+      return { summary };
+    },
+  );
 
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
