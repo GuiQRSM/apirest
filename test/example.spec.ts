7580a44cc330448291302f8b9af7c50467b1d1ec
GuiQRSM <quaresmagui20@gmail.com>
2025-11-13 12:25:58 -0300

    refactor: turns the functions async

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -2,7 +2,7 @@ import { expect, test } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app.js';
 
-test('o usuário consegue criar uma nova transação', () => {
+test('o usuário consegue criar uma nova transação', async () => {
   await request(app.server).post('/transactions').send({
     title: 'New Transaction',
     amount: 5000,
