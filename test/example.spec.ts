b2a01cfca4858a5ec69e51f94d96ed84713cf423
GuiQRSM <quaresmagui20@gmail.com>
2025-11-13 12:22:33 -0300

    add: call the native Node server to run tests.

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -3,7 +3,5 @@ import request from 'supertest';
 import { app } from '../src/app.js';
 
 test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201;
-
-  expect(responseStatusCode).toEqual(201);
+  await request(app.server);
 });
