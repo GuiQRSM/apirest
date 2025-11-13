4261f1ec84fe965b3647b43f27027cf27dab11f6
GuiQRSM <quaresmagui20@gmail.com>
2025-11-13 12:28:30 -0300

    add: declares the validator '.expect'

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -3,9 +3,12 @@ import request from 'supertest';
 import { app } from '../src/app.js';
 
 test('o usuário consegue criar uma nova transação', async () => {
-  await request(app.server).post('/transactions').send({
-    title: 'New Transaction',
-    amount: 5000,
-    type: 'credit',
-  });
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201);
 });
