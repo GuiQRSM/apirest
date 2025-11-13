63f40236d15196558b3ae837b9aaed5e2ac08dca
GuiQRSM <quaresmagui20@gmail.com>
2025-11-13 12:37:33 -0300

    refactor: remove the import from 'expect' and change the test statement to English.

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test, beforeAll, afterAll } from 'vitest';
+import { test, beforeAll, afterAll } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app.js';
 
@@ -8,7 +8,7 @@ beforeAll(async () => {
 
 afterAll(async () => await app.close());
 
-test('o usuário consegue criar uma nova transação', async () => {
+test('user can create a new transaction', async () => {
   await request(app.server)
     .post('/transactions')
     .send({
